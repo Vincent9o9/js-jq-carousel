e7b5e513c64e18066bb6f7d05ce10908769f9cf8
Vincent <23vincent86@gmail.com>
2020-07-23 16:08:36 +0200

    next

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
+    $(".prev").click(function(){
+        prevImage();
+    });
+
+    $(".next").click(function(){
+        nextImage();
+    });
+
+    function nextImage() {
+        //memorizzare in una variabile l'immagine attiva
+        var activeImage = $('.images img.active');
+
+        //togliera la classe
+        activeImage.removeClass('active');
+
+        //mettere la classe
+
+        activeImage.next().addClass('active');
+            
+    };
 
 
 
@@ -10,5 +30,4 @@ $(document).ready(function(){
 
 
 
-    
 });
