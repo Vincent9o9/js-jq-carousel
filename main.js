9ce8d875d69382cfbbdde19b766244645d041447
Vincent <23vincent86@gmail.com>
2020-07-23 17:00:55 +0200

    end

--- main.js
+++ main.js
@@ -8,25 +8,50 @@ $(document).ready(function(){
         nextImage();
     });
 
+    //******FUNZIONI******//
+
+    //funzione next//
+
     function nextImage() {
         //memorizzare in una variabile l'immagine attiva
         var activeImage = $('.images img.active');
+        var cerchio = $('.nav i.active');
 
         //togliera la classe
         activeImage.removeClass('active');
+        cerchio.removeClass('active');
 
         //mettere la classe
 
         activeImage.next().addClass('active');
-            
+
+        if (activeImage.hasClass('last')){
+            $('.images img.first').addClass('active');
+            $('.nav i.first').addClass('active');
+        } else {
+            activeImage.next().addClass('active')
+            cerchio.next().addClass('active')
+        }
     };
 
+    //funzione prev//
+
+    function prevImage() {
+        var activeImage = $('.images img.active');
+        var cerchio = $('.nav i.active');
+
+        activeImage.removeClass('active');
+        cerchio.removeClass('active');
 
 
-
-
-
-
+        if (activeImage.hasClass('first')){
+            $('.images img.last').addClass('active');
+            $('.nav i.last').addClass('active');
+        }else {
+            activeImage.prev().addClass('active');
+            cerchio.prev().addClass('active');
+        }
+    };
 
 
 
